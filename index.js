cc9e015d6a202a58ecc56722c29a07b981138dee
Rayhan Kobir <52678141+rajurayhan37@users.noreply.github.com>
2023-02-07 16:11:29 +0600

    cors mul

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000','http://localhost:5050'],
   credentials: true,
   optionsSuccessStatus: 200,
 }));
